a33961a193137cfe9c15b616903fbc5051162db1
Mieszko Wawrzyniak <kaaboaye@gmail.com>
2019-01-18 10:47:54 +0100

    remove static files

--- config/prod.exs
+++ config/prod.exs
@@ -5,7 +5,6 @@ get_env = fn key -> Map.fetch!(System.get_env(), key) end
 config :appman, AppmanWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: get_env.("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
